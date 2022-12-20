f260dbc4ac10a79a79a6730d99c667a61ed3f3a6
black6765 <black6765@gmail.com>
2022-12-20 11:01:30 +0900

    Update: Add & Modify source files

--- src/test/java/hello/core/MemberServiceTest.java
+++ src/test/java/hello/core/MemberServiceTest.java
@@ -3,13 +3,20 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    // 각 테스트 실행 전 실행되는 어노테이션
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
