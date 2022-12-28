aa5b6a035e11f5e8cd20211d420ef6a7e6d78407
black6765 <black6765@gmail.com>
2022-12-28 19:50:08 +0900

    Update: Add & Modify codes

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,12 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -39,5 +42,22 @@ public class SingletonTest {
         // isSameAs : == 문법을 이용한 비교(인스턴스가 같은지 비교)
         // isEqual  : 자바의 equals() 메소드를 이용한 비교
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberServiceImpl.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberServiceImpl.class);
+
+        // 참조 값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 == memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
     }
 }
